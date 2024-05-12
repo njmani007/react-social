873c2006abbaaa7ed0af2bb26537b64ba67aee52
Jothimani_N <njm48419@gmail.com>
2024-05-12 15:58:40 +0530

    8th-commit

--- frontend/src/scenes/widgets/AdvertWidget.jsx
+++ frontend/src/scenes/widgets/AdvertWidget.jsx
@@ -22,8 +22,7 @@ const AdvertWidget = () => {
     height="auto"
     alt="advert"
      className="advert-image"
-    src={"${API_BASE_URL}/assets/info4.jpeg"}
-    // style="border-radius: 0.75rem; margin: 0.75rem 0;"
+     src={`${API_BASE_URL}/assets/info4.jpeg`}
 />
       <FlexBetween>
         <Typography color={main}>MikaCosmetics</Typography>
